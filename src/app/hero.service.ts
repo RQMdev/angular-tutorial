c9e30c631c6d52016aa06b0f3b04c6302d0644b1
RQMplays <benoit.jacques.felix@gmail.com>
2017-12-04 17:02:55 +0100

    follow Tour Of Heroes tutorial, still in 8.HTTP Error Handling

---
+++ src/app/hero.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable()
+export class HeroService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    // Todo: send the message _after_ fetching the heroes
+    return this.http.get<Hero[]>(this.heroesUrl)
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+}
